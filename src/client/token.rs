d5a3a871cffec80078d294577f50280ac0714ddb
Ilya Zlobintsev <ilya.zl@protonmail.com>
2026-06-25 15:29:40 +0300

    perf: cache Azure GetUserDelegationKey calls
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- src/client/token.rs
+++ src/client/token.rs
@@ -52,7 +52,7 @@ impl<T> Default for TokenCache<T> {
 
 impl<T: Clone + Send> TokenCache<T> {
     /// Override the minimum remaining TTL for a cached token to be used
-    #[cfg(any(feature = "aws", feature = "gcp"))]
+    #[cfg(any(feature = "aws", feature = "azure", feature = "gcp"))]
     pub(crate) fn with_min_ttl(self, min_ttl: Duration) -> Self {
         Self { min_ttl, ..self }
     }
